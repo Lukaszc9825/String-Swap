9d864934bc156a81680373fc53c22ca47c3a7ff2
Łukasz Cieśluk <lukaszciesluk98@gmail.com>
2019-06-19 16:15:32 +0200

    add comments in file

--- src/File_reader.java
+++ src/File_reader.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.Scanner;
 
 public class File_reader {
+
+     private File file;
+     private Scanner scanner;
+
+     // function which Read string from file
+     public void Read()
+     {
+         try {
+             scanner = new Scanner(getFile());
+             String lane = null;
+
+             do {
+                 if (scanner.hasNext("#")) //# ..... allows to comment in file
+                 {
+                     scanner.nextLine();
+
+                 }
+                 lane = scanner.nextLine();
+                 System.out.println(lane);
+             }while (lane != null);
+         }catch(Exception ex)
+         {
+
+         }finally {
+             try {
+                 scanner.close();
+             }catch (Exception ex)
+             {
+                 System.out.println("Problem with file");
+             }
+         }
+     }
+
+    public void setFile(String f)
+    {
+        this.file=new File(f);
+    }
+
+    public File getFile()
+    {
+       return this.file;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main (String[] args)
     {
-        
+        File_reader f = new File_reader();
+        f.setFile("file1.txt");
+        f.Read();
     }
 }
